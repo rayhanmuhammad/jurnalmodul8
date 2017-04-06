a231c489797413a08e5b911193d526d9d4b5d616
rayhan muhammad <rayhanmuh98@gmail.com>
2017-04-06 09:12:33 +0700

    Update Jurnal.java

--- Jurnal.java
+++ Jurnal.java
@@ -20,17 +20,19 @@ public class Jurnal {
         // TODO code application logic here
          Scanner sc=new Scanner(System.in);
          int n1=1,n2=1,n3,i; 
+        int hasil=n1+n2;
          System.out.println("Masukkan Inputan : ");
          int count=sc.nextInt();
-         System.out.print(n1+" "+n2);
+         
     
         for(i=2;i<count;++i)
         {    
         n3=n1+n2;    
-        System.out.print(" "+n3);    
+        hasil+=n3;   
         n1=n2;    
         n2=n3;    
-        }    
+        }
+        System.out.print(hasil);
     }
     
 }
